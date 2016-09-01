fc2ced04dc6efbdff96e430cadb6b9b7b67212b6
Christian <smoki99@googlemail.com>
2016-09-01 21:59:06 +0200

    adding servo

--- server.js
+++ server.js
@@ -5,9 +5,31 @@ var Server = (function () {
     // Start the configuration
     function Server() {
         var ledstatus="on";
+        var servoangle1 = 1;
+        var servoangle2 = 2;
         this.app = express();
         this.app.use("/lib", express.static(path.resolve(__dirname, "lib")));
         this.app.use("/", express.static(path.resolve(__dirname, "views")));
+        this.app.get("/servoangle1", function (req, res) {
+            res.send("Servo1: "+servoangle1);
+        });
+        this.app.get("/servoangle2", function (req, res) {
+            res.send("Servo2: "+servoangle2);
+        });
+        this.app.get("/servoleft1", function (req, res) {
+            servoangle1-=10;
+            if (servoangle1<=0)
+                servoangle1=1;
+            res.send('ok');
+        });
+        this.app.get("/servoright1", function (req, res) {
+            servoangle1+=10;
+            if (servoangle1>=128)
+                servoangle1=127;
+            res.send('ok');
+        });
+
+
         this.app.get("/ledstatus", function (req, res) {
             res.send(ledstatus);
         });
